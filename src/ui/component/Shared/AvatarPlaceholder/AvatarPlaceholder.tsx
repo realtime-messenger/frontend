709b2b74fc6525d4b19262e224dc88631a302c18
enk-it <gladysh.danil@gmail.com>
2025-04-25 03:21:36 +0700

    feat: Add indicator for online

--- src/ui/component/Shared/AvatarPlaceholder/AvatarPlaceholder.tsx
+++ src/ui/component/Shared/AvatarPlaceholder/AvatarPlaceholder.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import {FC} from 'react';
 import classes from "./AvatarPlaceholder.module.css";
+import {ONLINE_THRESHOLD} from "../../../../config.ts";
 
 
 interface AvatarPlaceholderProps {
 	firstName?: string
 	lastName?: string
 	title?: string
+	isOnline?: boolean
 }
 
 const AvatarPlaceholder: FC<AvatarPlaceholderProps> = (
 	{
 		firstName,
 		lastName,
-		title
+		title,
+		isOnline
 	}
 ) => {
 
@@ -21,13 +24,18 @@ const AvatarPlaceholder: FC<AvatarPlaceholderProps> = (
 			return title[0].toUpperCase()
 		}
 		else {
-			return (firstName[0] + lastName[0]).toUpperCase();
+			return (firstName![0] + lastName![0]).toUpperCase();
 		}
 	}
 
 	return (
 		<div className={classes.avatarPlaceholder}>
 			{getText()}
+			{
+				isOnline && (
+					<div className={classes.onlineIndicator}/>
+				)
+			}
 		</div>
 	)
 
